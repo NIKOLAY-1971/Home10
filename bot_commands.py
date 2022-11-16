18d8275caa9ba71a2e9cc2e2e66134a89874eb37
Nikolay <rnm71@yandex.ru>
2022-11-16 22:30:05 +0300

    add home10_2

--- bot_commands.py
+++ bot_commands.py
@@ -1,27 +1,11 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import random
+import sympy
 
 async def sum_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     msg = update.message.text
     items = msg.split()
-    k = int(items[1])
- 
-    lst = []
-    lstNew = []
-    for i in range(k+1):
-        lst.append(random.randint(0, 100))
-    for i in lst:
-        if i:
-            if k == 0:
-                variable = ''
-            elif k == 1:
-                variable = 'x'
-            else:
-                variable = f'x^{k}'
-            variable_k = f'{i}{variable}'
-            lstNew.append(variable_k)
-        k = k-1
-    polinom = '+'.join(lstNew)+'=0'
-        
-    await update.message.reply_text(f'Полином:\n {polinom}')
+    polinom1 = sympy.polys.polytools.poly_from_expr(str(items[1]))[0]
+    polinom2 = sympy.polys.polytools.poly_from_expr(str(items[2]))[0]
+    sumPolinom = str((polinom1+polinom2).as_expr()).replace('**','^')+' = 0'    
+    await update.message.reply_text(f'Сумма полиномов:\n {sumPolinom}')
--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 from bot_commands import *
 
-
-
-
 app = ApplicationBuilder().token("5610197004:AAFnwUAMiNR2p82oQ7uPTa2YrFxqL3no0a4").build()
 
 app.add_handler(CommandHandler("sum", sum_command))
